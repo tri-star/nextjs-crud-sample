ba8a417e808d261e506864ad6f44cd382b71ad2c
tristar <tristar.2nd@gmail.com>
2022-10-07 06:07:00 +0900

    細かな調整

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    reactRemoveProperties: { properties: ['^data-test'] },
+    removeConsole: {
+      exclude: ['error'],
+    },
+  }
 }
 
 module.exports = nextConfig
