1f381df6b9e404231efc96916dd9172d81b9c3b6
tristar <tristar.2nd@gmail.com>
2022-10-09 08:54:41 +0900

    muiの導入

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Head, Html, Main, NextScript } from "next/document"
+
+const Doc = () => {
+  return (
+    <Html lang="ja">
+      <Head>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      </Head>
+      <body>
+        <Main/>
+        <NextScript/>
+      </body>
+    </Html>
+  )
+}
+
+export default Doc
