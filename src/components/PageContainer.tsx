f1d1e993efca2cf2c68dc638ff85b3358e30c00b
tristar <tristar.2nd@gmail.com>
2022-10-18 01:25:02 +0900

    ページの高さを100vh-ヘッダ分するコンポーネントを共通化。

---
+++ src/components/PageContainer.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material"
+import { PropsWithChildren, ReactElement } from "react"
+
+
+export const PageContainer = ({children}: PropsWithChildren): ReactElement => {
+  return (
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      maxHeight: 'calc(100vh - 100px)'
+    }}>
+      {children}
+    </Box>
+  )
+}
