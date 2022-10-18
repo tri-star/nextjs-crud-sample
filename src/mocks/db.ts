9078bfa037754e2b3e6ac47f2e7b78073b68bc25
tristar <tristar.2nd@gmail.com>
2022-10-19 05:22:29 +0900

    mswのDBからレスポンスを返すように変更

---
+++ src/mocks/db.ts
@@ -0,0 +1,61 @@
+import { isServer } from "@/common/ssr";
+import { factory, primaryKey } from "@mswjs/data";
+
+export const mockDb = factory({
+  user: {
+    id: primaryKey(Number),
+    loginId: String,
+    name: String,
+    email: String,
+    departmentId: Number
+  }
+})
+
+type Model = keyof typeof mockDb
+
+export const loadDb = () => {
+  if (isServer()) {
+    return
+  }
+  return Object.assign(JSON.parse(window.localStorage.getItem('msw-db') || '{}'))
+}
+
+export const persistDb = (model: Model) => {
+  if (process.env.NODE_ENV === 'test' || isServer()) {
+    return
+  }
+  const data = loadDb()
+  data[model] = mockDb[model]
+  window.localStorage.setItem('msw-db', JSON.stringify(data))
+}
+
+
+export const initDb = () => {
+  if (isServer()) {
+    return
+  }
+  const db = loadDb()
+  Object.entries(mockDb).forEach(([key, model]) => {
+    const rows = db[key]
+    if (rows) {
+      rows.forEach((row: Record<string, any>) => {
+        model.create(row)
+      })
+    }
+  })
+
+  if (mockDb.user.count() === 0) {
+    [...Array(500)].forEach((_, i) => {
+      const id = i + 1
+      mockDb.user.create({
+        id: id,
+        loginId: `user_${id}`,
+        name: `ユーザー${id}`,
+        email: `test${id}@example.com`,
+        departmentId: 1,
+      })
+    })
+  }
+}
+
+initDb()
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,25 +1,44 @@
 import { appConfig } from "@/app-config"
-import { mockFetchUserDetail } from "@/features/users/api/fetch-user-detail"
-import { mockFetchUserList } from "@/features/users/api/fetch-user-list"
 import { RequestHandler, rest } from "msw"
+import { mockDb } from "./db"
 
 const apiBase = appConfig.apiBase
 
 export const handlers: RequestHandler[] = [
   rest.get(`${apiBase}/admin/users`, (req, res, ctx) => {
 
-    const page = req.url.searchParams.get('page') ?? '1'
+    const page = +(req.url.searchParams.get('page') ?? '1')
+    const pageSize = 50
+    const totalCount = mockDb.user.count()
+    const pages = Math.trunc(totalCount / pageSize) + ((totalCount % pageSize == 0) ? 1 : 0)
 
-    const result = mockFetchUserList(+page)
-    return res(ctx.status(200), ctx.json(result))
+    const users = mockDb.user.findMany({
+      take: 50,
+      skip: (page - 1) * pageSize
+    })
+    return res(ctx.status(200), ctx.json({
+      users,
+      count: mockDb.user.count(),
+      pages,
+      page: page,
+    }))
   }),
   rest.get(`${apiBase}/admin/users/:id`, (req, res, ctx) => {
 
     const { id: userId } = req.params
 
     try {
-      const result = mockFetchUserDetail(+userId)
-      return res(ctx.status(200), ctx.json(result))
+      const user = mockDb.user.findFirst({
+        where: {
+          id: {
+            equals: +userId
+          }
+        }
+      })
+      if (!user) {
+        throw new Error('Not found')
+      }
+      return res(ctx.status(200), ctx.json(user))
     } catch (e) {
       if (e instanceof Error) {
         return res(ctx.status(404), ctx.json(e))
