660aac9ab170782b52f58789fa56d2d8b736957f
tristar <tristar.2nd@gmail.com>
2022-10-26 05:28:12 +0900

    ロード中の制御のHookを追加

---
+++ src/common/loading.ts
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+
+export const useLoading = () => {
+  const [loading, setLoading] = useState(false)
+
+  const withLoading = async <T>(callback: () => Promise<T>) => {
+    setLoading(true)
+    try {
+      await callback()
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return {
+    loading,
+    withLoading
+  }
+}
