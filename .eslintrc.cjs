028c81f3d948b483395f2be893c9a4afce83537f
tristar <tristar.2nd@gmail.com>
2022-11-03 11:08:01 +0900

    msw/node初期化時のエラーに対処

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -42,7 +42,6 @@ module.exports = {
     "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-var-requires": "off",
   },
   "settings": {
     "react": {
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,12 +1,16 @@
 import { handlers } from './handlers'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const mswNode = require('msw/node')
 
+// @ts-expect-error mswNodeがnamespace not foundでエラーになるため
 let server: mswNode.SetupServerApi | undefined
 
 export const startMockServer = () => {
   if (server == null) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     server = mswNode.setupServer(...handlers)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     server.listen()
   }
   return server
